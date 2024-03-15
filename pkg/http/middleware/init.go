7c65ed7fd97cda56f758c5a8bbc4fe2bd296e313
Azie Melza Pratama <aziemp66@gmail.com>
2024-03-15 15:30:49 +0700

    feat : Converting to Return Condition

--- pkg/http/middleware/init.go
+++ pkg/http/middleware/init.go
@@ -14,28 +14,37 @@ func ErrorHandler() gin.HandlerFunc {
 		c.Writer.Header().Add("Content-Type", "application/json")
 		c.Next()
 
-		if len(c.Errors) > 0 {
-			err := c.Errors[0]
-			// if err can be casted to ClientError, then it is a client error
-			if clientError, ok := err.Err.(*errorPkg.ClientError); ok {
-				c.JSON(clientError.Code, httpPkg.Error{
-					Message: clientError.Message,
-				})
-			} else if err.IsType(gin.ErrorTypeBind) {
-				c.JSON(400, httpPkg.Error{
-					Message: err.Err.Error(),
-				})
-			} else if err.IsType(gin.ErrorTypePrivate) {
-				fmt.Println(err.Err.Error())
-				c.JSON(500, httpPkg.Error{
-					Message: "Internal server error",
-				})
-			} else {
-				fmt.Println(err.Err.Error())
-				c.JSON(500, httpPkg.Error{
-					Message: "Internal server error",
-				})
-			}
+		if len(c.Errors) < 1 {
+			return
 		}
+
+		err := c.Errors[0]
+		// if err can be casted to ClientError, then it is a client error
+		if clientError, ok := err.Err.(*errorPkg.ClientError); ok {
+			c.JSON(clientError.Code, httpPkg.Error{
+				Message: clientError.Message,
+			})
+			return
+		}
+
+		if err.IsType(gin.ErrorTypeBind) {
+			c.JSON(400, httpPkg.Error{
+				Message: err.Err.Error(),
+			})
+			return
+		}
+
+		if err.IsType(gin.ErrorTypePrivate) {
+			fmt.Println(err.Err.Error())
+			c.JSON(500, httpPkg.Error{
+				Message: "Internal server error",
+			})
+			return
+		}
+
+		fmt.Println(err.Err.Error())
+		c.JSON(500, httpPkg.Error{
+			Message: "Internal server error",
+		})
 	}
 }
