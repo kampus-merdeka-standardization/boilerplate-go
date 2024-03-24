9d4d17a617b72fe16a61b9cc5f710a7040947d8c
Azie Melza Pratama <aziemp66@gmail.com>
2024-03-24 22:01:04 +0700

    feat : Changing Graphql Flow

--- cmd/graphql/main.go
+++ cmd/graphql/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
 	pinger_graphql "github.com/kampus-merdeka-standardization/boilerplate-go/internal/pinger/handler/graphql"
 	"github.com/kampus-merdeka-standardization/boilerplate-go/pkg/configs"
 	httpPkg "github.com/kampus-merdeka-standardization/boilerplate-go/pkg/http"
@@ -19,10 +23,32 @@ func main() {
 	srv.Use(middleware.LogHandler(), gin.Recovery())
 	srv.Use(middleware.CorsHandler())
 
-	srv.POST("/pinger", pinger_graphql.NewPingerHandler)
+	srv.Handle(http.MethodGet, "", gin.WrapH(handler.New(&handler.Config{
+		Schema:     graphqlSchema(),
+		Pretty:     true,
+		Playground: true,
+	})))
 
 	logger.Info("Running on Port " + conf.Port)
 	if err := srv.Run(":" + conf.Port); err != nil {
 		logger.Fatal(err.Error())
 	}
+}
+
+func graphqlSchema() *graphql.Schema {
+	obj := graphql.NewObject(graphql.ObjectConfig{
+		Name: "Root",
+		Fields: graphql.Fields{
+			"ping": pinger_graphql.NewField(),
+		},
+	})
+
+	schema, err := graphql.NewSchema(graphql.SchemaConfig{
+		Query: obj,
+	})
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	return &schema
 }
--- internal/pinger/handler/graphql/init.go
+++ internal/pinger/handler/graphql/init.go
@@ -1,52 +1,16 @@
 package pinger_graphql
 
 import (
-	"log"
-
-	"github.com/gin-gonic/gin"
 	"github.com/graphql-go/graphql"
-	httpPkg "github.com/kampus-merdeka-standardization/boilerplate-go/pkg/http"
 )
 
-func NewPingerHandler(ctx *gin.Context) {
-	var query struct {
-		Query string `json:"query" binding:"required"`
-	}
-
-	if err := ctx.ShouldBindJSON(&query); err != nil {
-		ctx.Error(err)
-		return
-	}
-
-	result := graphql.Do(graphql.Params{
-		Schema:        newSchema(),
-		RequestString: query.Query,
-	})
-
-	ctx.JSON(200, httpPkg.Response{
-		Message: "Successfully Retrieved Query Response",
-		Value:   result,
-	})
-}
-
-func newSchema() graphql.Schema {
+func NewField() *graphql.Field {
 	ql := &pingerGraphql{}
 
-	rootQuery := graphql.NewObject(graphql.ObjectConfig{
-		Name: "RootQuery",
-		Fields: graphql.Fields{
-			"ping": &graphql.Field{
-				Type:    graphql.String,
-				Resolve: ql.Ping,
-			},
-		},
-	})
-
-	schema, err := graphql.NewSchema(graphql.SchemaConfig{
-		Query: rootQuery,
-	})
-	if err != nil {
-		log.Fatal(err)
+	pinger := &graphql.Field{
+		Type:    graphql.String,
+		Resolve: ql.Ping,
 	}
-	return schema
+
+	return pinger
 }
