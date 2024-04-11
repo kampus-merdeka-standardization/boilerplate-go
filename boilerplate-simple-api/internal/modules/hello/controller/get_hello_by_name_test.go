9cb8a78b093e5f77adf942e860aa60f81362d4e6
Azie Melza Pratama <aziemp66@gmail.com>
2024-04-11 22:51:11 +0700

    feat : Adding Get Hello By Name test

---
+++ boilerplate-simple-api/internal/modules/hello/controller/get_hello_by_name_test.go
@@ -0,0 +1,33 @@
+package hello_controller_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	pkg_http_wrapper "simple-api/pkg/http/wrapper"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetHelloByName(t *testing.T) {
+	srv := setupTest()
+
+	t.Run("Get Hello By Name Http Test 1", func(t *testing.T) {
+		res := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, "/hello/azie", nil)
+
+		srv.ServeHTTP(res, req)
+
+		name := "azie"
+		messageExpect := fmt.Sprintf("Hello, %s!", name)
+
+		resBody := pkg_http_wrapper.NewResponse("", "")
+
+		err := json.Unmarshal(res.Body.Bytes(), &resBody)
+		assert.Nil(t, err)
+
+		assert.Equal(t, messageExpect, resBody.Message)
+	})
+}
