a262052e5d5a321ecab9e075e7e9a1ea2d579612
Azie Melza Pratama <aziemp66@gmail.com>
2024-04-13 14:01:02 +0700

    feat : Complete Sent Request Unit Test

--- rest-client-example/rest-client/sent_request_test.go
+++ rest-client-example/rest-client/sent_request_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSentRequest(t *testing.T) {
@@ -14,40 +15,53 @@ func TestSentRequest(t *testing.T) {
 
 	t.Run("Get Request Test", func(t *testing.T) {
 		resp, code, err := client.SendRequest(http.MethodGet, "/objects/3", nil, nil)
-		assert.Nil(t, err)
+		require.Nil(t, err)
 		assert.Equal(t, 200, code)
-		assert.NotNil(t, resp)
+		require.NotNil(t, resp)
 	})
 	t.Run("Post Request Test", func(t *testing.T) {
+		type bodyData struct {
+			Year         int     `json:"year"`
+			Price        float64 `json:"price"`
+			CPUModel     string  `json:"CPU model"`
+			HardDiskSize string  `json:"Hard disk size"`
+		}
 		type bodyRequest struct {
-			Name string `json:"name"`
-			Year int    `json:"year"`
+			Name string   `json:"name"`
+			Data bodyData `json:"data"`
 		}
 
 		reqBody := bodyRequest{
-			Name: "Toyota Corolla",
-			Year: 1998,
+			Name: "Lenovo Thinkpad 5",
+			Data: bodyData{
+				Year:         2021,
+				Price:        599.99,
+				CPUModel:     "Intel Core i5 Gen 10",
+				HardDiskSize: "1 TB",
+			},
 		}
 
 		resp, code, err := client.SendRequest(http.MethodPost, "/objects", reqBody, map[string]string{
 			"content-type": "application/json",
 		})
-		assert.Nil(t, err)
+		require.Nil(t, err)
 		assert.Equal(t, 200, code)
-		assert.NotNil(t, resp)
+		require.NotNil(t, resp)
 
 		type bodyResponse struct {
-			Id        string `json:"id"`
-			Name      string `json:"name"`
-			Year      int    `json:"year"`
-			CreatedAt string `json:"createdAt"`
+			Id        string   `json:"id"`
+			Name      string   `json:"name"`
+			Data      bodyData `json:"data"`
+			CreatedAt string   `json:"createdAt"`
 		}
 
 		respBody := bodyResponse{}
 		err = json.Unmarshal(resp, &respBody)
-		assert.Nil(t, err)
+		require.Nil(t, err)
 
-		assert.NotEqual(t, "", respBody.Id)
-		assert.NotEqual(t, "", respBody.CreatedAt)
+		assert.NotEmpty(t, respBody.Id)
+		assert.NotEmpty(t, respBody.CreatedAt)
+		assert.Equal(t, reqBody.Name, respBody.Name)
+		assert.Equal(t, reqBody.Data, respBody.Data)
 	})
 }
