70ef535051951de04d2019a3799b054abdf25a38
Azie Melza Pratama <aziemp66@gmail.com>
2024-06-30 18:01:04 +0700

    fix : Updating database query

--- simple-golang-database/internal/modules/product/repository/postgres/const.go
+++ simple-golang-database/internal/modules/product/repository/postgres/const.go
@@ -2,8 +2,8 @@ package product_postgres
 
 const (
 	createProduct     = `INSERT INTO products (name, price) VALUES ($1, $2) RETURNING id, name, price`
-	getProductByID    = `SELECT * FROM products WHERE id = $1`
-	getAllProduct     = `SELECT * FROM products`
+	getProductByID    = `SELECT id, name, price FROM products WHERE id = $1`
+	getAllProduct     = `SELECT id, name, price FROM products`
 	updateProductByID = `UPDATE products set name = $2, price = $3 WHERE id = $1 RETURNING id, name, price`
 	deleteProductByID = `DELETE FROM products where id = $1`
 )
