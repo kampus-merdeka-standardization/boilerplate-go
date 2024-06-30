7fc0ee4a6ea68dcd8be21ae7b4c8969d43d7bfd1
Azie Melza Pratama <aziemp66@gmail.com>
2024-06-30 16:10:04 +0700

    fix : Remove unnecessary New Error Function

--- simple-golang-database/pkg/http/middleware/error.go
+++ simple-golang-database/pkg/http/middleware/error.go
@@ -24,23 +24,23 @@ func ErrorHandler() gin.HandlerFunc {
 		// if err can be casted to ClientError, then it is a client error
 		if clientError, ok := err.Err.(*errorPkg.ClientError); ok {
 			pkg_logger.Error(clientError.Raw.Error(), zap.Int("Code", clientError.Code))
-			c.JSON(clientError.Code, pkg_http_wrapper.NewError(clientError.Code, clientError.Message))
+			c.JSON(clientError.Code, pkg_http_wrapper.NewResponse(clientError.Code, clientError.Message))
 			return
 		}
 
 		if err.IsType(gin.ErrorTypeBind) {
 			pkg_logger.Error(err.Error(), zap.Any("Error", err))
-			c.JSON(400, pkg_http_wrapper.NewError(http.StatusBadRequest, err.Err.Error()))
+			c.JSON(400, pkg_http_wrapper.NewResponse(http.StatusBadRequest, err.Err.Error()))
 			return
 		}
 
 		if err.IsType(gin.ErrorTypePrivate) {
 			pkg_logger.Error(err.Error(), zap.Any("Error", err))
-			c.JSON(500, pkg_http_wrapper.NewError(http.StatusInternalServerError, "Internal Server Error"))
+			c.JSON(500, pkg_http_wrapper.NewResponse(http.StatusInternalServerError, "Internal Server Error"))
 			return
 		}
 
 		pkg_logger.Error(err.Error(), zap.Any("Error", err))
-		c.JSON(500, pkg_http_wrapper.NewError(http.StatusInternalServerError, "Internal Server Error"))
+		c.JSON(500, pkg_http_wrapper.NewResponse(http.StatusInternalServerError, "Internal Server Error"))
 	}
 }
